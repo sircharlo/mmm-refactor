4aab07a8b1b983672feea6373b5a263694633bdc
Olivier Savignac <sircharlo@gmail.com>
2024-06-23 10:39:39 -0400

    fix: try with router integration

--- src/boot/sentry.ts
+++ src/boot/sentry.ts
@@ -1,25 +1,24 @@
 import * as Sentry from '@sentry/vue';
-import path from 'path';
+// import path from 'path';
 import { boot } from 'quasar/wrappers';
 
 import packageInfo from '../../package.json';
 
-export default boot(({ app }) => {
+export default boot(({ app, router }) => {
   Sentry.init({
     app,
     dsn: 'https://0f2ab1c7ddfb118d25704c85957b8188@o1401005.ingest.us.sentry.io/4507449197920256',
     integrations: [
-      Sentry.rewriteFramesIntegration({
-        // @ts-expect-error Frame typing
-        iteratee: (frame) => {
-          frame.filename = frame.filename
-            ? path.basename(frame.filename)
-            : frame.filename;
-        },
-        prefix: '',
-      }),
-      Sentry.vueIntegration(),
-      Sentry.browserTracingIntegration(),
+      // Sentry.rewriteFramesIntegration({
+      //   // @ts-expect-error Frame typing
+      //   iteratee: (frame) => {
+      //     frame.filename = frame.filename
+      //       ? path.basename(frame.filename)
+      //       : frame.filename;
+      //   },
+      //   prefix: '',
+      // }),
+      Sentry.browserTracingIntegration({ router }),
       Sentry.replayIntegration(),
     ],
     release: packageInfo.version,
