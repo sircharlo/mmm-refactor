9f0d5126ccdae2fa71ebee2988b352f0f1c586b7
Olivier Savignac <sircharlo@gmail.com>
2024-06-23 10:00:28 -0400

    fix: build is getting closer..

--- src/boot/sentry.ts
+++ src/boot/sentry.ts
@@ -8,10 +8,10 @@ export default boot(({ app }) => {
     app,
     dsn: 'https://0f2ab1c7ddfb118d25704c85957b8188@o1401005.ingest.us.sentry.io/4507449197920256',
     integrations: [
+      Sentry.rewriteFramesIntegration({ prefix: '~/' }),
       Sentry.vueIntegration(),
       Sentry.browserTracingIntegration(),
       Sentry.replayIntegration(),
-      Sentry.rewriteFramesIntegration(),
     ],
     release: packageInfo.version,
     replaysOnErrorSampleRate: 1.0,
