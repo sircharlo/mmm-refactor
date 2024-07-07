34260e40ccfc6c9c5322ff2dbb906e587adbe74c
Olivier Savignac <1275666+sircharlo@users.noreply.github.com>
2024-07-07 19:01:50 -0400

    fix: separate media scene from others in OBS scene picker

--- src/components/media/ObsStatus.vue
+++ src/components/media/ObsStatus.vue
@@ -36,14 +36,39 @@
         <q-card-section>
           <div class="text-h6">{{ $t('scene-selection') }}</div>
         </q-card-section>
-
+        <q-card-section>
+          <div class="row q-col-gutter-md">
+            <div
+              :class="
+                'col-' +
+                ([mediaScene].concat(nonMediaScenes).filter(Boolean).length < 4
+                  ? (
+                      12 /
+                      [mediaScene].concat(nonMediaScenes).filter(Boolean).length
+                    ).toString()
+                  : '3')
+              "
+            >
+              <q-btn
+                :color="
+                  currentSceneUuid === mediaScene?.sceneUuid
+                    ? 'warning'
+                    : 'primary'
+                "
+                :label="mediaScene?.sceneName as string"
+                @click="setObsSceneByUuid(mediaScene?.sceneUuid as string)"
+                class="q-py-md q-px-lg full-width"
+                size="1.5em"
+              />
+            </div>
+          </div>
+        </q-card-section>
+        <q-separator />
         <q-card-section>
           <div class="row q-col-gutter-md">
             <template
               :key="scene.sceneUuid"
-              v-for="scene in [mediaScene]
-                .concat(nonMediaScenes)
-                .filter(Boolean)"
+              v-for="scene in nonMediaScenes.filter(Boolean)"
             >
               <div
                 :class="
@@ -228,8 +253,6 @@ onUnmounted(() => {
   try {
     window.removeEventListener('obsConnectFromSettings', setObsSceneListener);
     window.removeEventListener('obsSceneEvent', setObsSceneListener);
-
-
     obsWebSocket.removeAllListeners('ConnectionClosed');
     obsWebSocket.removeAllListeners('ConnectionError');
     obsWebSocket.removeAllListeners('CurrentProgramSceneChanged');
