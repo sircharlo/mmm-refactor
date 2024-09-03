f8c2d3b4632c93fe4e0ac65b5e1b57a0b4ad250a
Olivier Savignac <1275666+sircharlo@users.noreply.github.com>
2024-09-03 15:55:01 -0400

    fix: (re-)implement cleanup, but functional this time

--- src/helpers/cleanup.ts
+++ src/helpers/cleanup.ts
@@ -1,120 +1,112 @@
-// import { storeToRefs } from 'pinia';
-// import { date, LocalStorage } from 'quasar';
-// import { isInPast } from 'src/helpers/date';
-// import { electronApi } from 'src/helpers/electron-api';
-// import { useJwStore } from 'src/stores/jw';
-// import { DynamicMediaObject } from 'src/types/media';
-import { errorCatcher } from './error-catcher';
-// import { getAdditionalMediaPath, removeEmptyDirs } from './fs';
+import { storeToRefs } from 'pinia';
+import { LocalStorage } from 'quasar';
+import { isInPast } from 'src/helpers/date';
+import { electronApi } from 'src/helpers/electron-api';
+import { errorCatcher } from 'src/helpers/error-catcher';
+import { getAdditionalMediaPath, removeEmptyDirs } from 'src/helpers/fs';
+import { useJwStore } from 'src/stores/jw';
+import { DynamicMediaObject } from 'src/types/media';
 
-// const { fileUrlToPath, fs, klawSync, path } = electronApi;
+const { fileUrlToPath, fs, klawSync, path } = electronApi;
 
 const cleanLocalStorage = () => {
   try {
-    for (const storageElementName of [
-      'additionalMediaMaps',
-      'customDurations',
-      'mediaSort',
-    ]) {
-      // TODO: Implement correct and safe cleanup here
-      console.log('Checking:', storageElementName);
-      //   const storageElement = LocalStorage.getItem(storageElementName);
-      //   let changesMade = false;
-      //   if (storageElement) {
-      //     for (const congregation of Object.keys(storageElement)) {
-      //       // @ts-expect-error LocalStorage typing mishap here
-      //       console.log('Checking:', congregation, storageElementName, storageElement[congregation]);
-      //       // @ts-expect-error LocalStorage typing mishap here
-      //       for (const dateKey of Object.keys(storageElement[congregation])) {
-      //         console.log('Checking:', congregation, dateKey);
-      //         // if (!key) delete storageElement[key];
-      //         const strippedDateKey = dateKey.replace(/\D/g, '');
-      //         console.log(strippedDateKey);
-      //         // strippedKey = YYYYMMDD
-      //         const strippedDateKeyAsDate = date.buildDate(
-      //           {
-      //             day: Number(strippedDateKey.slice(6, 8)),
-      //             hours: 0,
-      //             minutes: 0,
-      //             month: Number(strippedDateKey.slice(4, 6)),
-      //             seconds: 0,
-      //             year: Number(strippedDateKey.slice(0, 4)),
-      //           },
-      //           false,
-      //         );
-      //         console.log(strippedDateKeyAsDate);
-      //         if (isInPast(strippedDateKeyAsDate)) {
-      //           console.log('Removing from localStorage:', congregation, storageElementNamedateKey);
-      //           // @ts-expect-error LocalStorage typing mishap here
-      //           delete storageElement[congregation][dateKey];
-      //           changesMade = true;
-      //         }
-      //       }
-      //     }
-      //   }
-      //   if (changesMade) LocalStorage.set(storageElementName, storageElement);
+    function cleanUpPastDatesFromLocalStorage(key: string) {
+      const data = LocalStorage.getItem(key);
+      if (!data) {
+        return;
+      }
+
+      Object.keys(data).forEach((uid) => {
+        // @ts-expect-error LocalStorage typing mishap here
+        const datesObj = data[uid];
+        Object.keys(datesObj).forEach((dateKey) => {
+          const dateValue = new Date(dateKey);
+          if (isInPast(dateValue)) {
+            delete datesObj[dateKey];
+          }
+        });
+
+        if (Object.keys(datesObj).length === 0) {
+          // @ts-expect-error LocalStorage typing mishap here
+          delete data[uid];
+        }
+      });
+
+      LocalStorage.set(key, data);
     }
+
+    ['customDurations', 'additionalMediaMaps', 'mediaSort'].forEach((key) => {
+      cleanUpPastDatesFromLocalStorage(key);
+    });
   } catch (error) {
     errorCatcher(error);
   }
 };
 
 const cleanAdditionalMediaFolder = () => {
-  // TODO: Implement correct and safe cleanup here
-  // try {
-  //   const jwStore = useJwStore();
-  //   const { additionalMediaMaps } = storeToRefs(jwStore);
-  //   const additionalMediaPath = getAdditionalMediaPath();
-  //   if (!fs.existsSync(additionalMediaPath)) {
-  //     additionalMediaMaps.value = {};
-  //     return;
-  //   }
-  //   const flattenedFilePaths = (
-  //     data: Record<string, Record<string, DynamicMediaObject[]>>,
-  //   ) => {
-  //     return Object.values(data).flatMap((dateObj) =>
-  //       Object.values(dateObj).flatMap((files) =>
-  //         files.map((file) => path.resolve(fileUrlToPath(file.fileUrl))),
-  //       ),
-  //     );
-  //   };
-  //   // Check for files present on the filesystem that are not present in the additional media maps
-  //   const filesReferencedInAdditionalMediaMaps = flattenedFilePaths(
-  //     additionalMediaMaps.value,
-  //   );
-  //   const dirListing = klawSync(additionalMediaPath, { nodir: true });
-  //   for (const file of dirListing) {
-  //     const additionalMediaFilePath = path.resolve(file.path);
-  //     if (
-  //       !filesReferencedInAdditionalMediaMaps.includes(additionalMediaFilePath)
-  //     ) {
-  //       console.log(
-  //         'Removing orphaned file from filesystem:',
-  //         additionalMediaFilePath,
-  //       );
-  //       fs.rmSync(additionalMediaFilePath);
-  //     }
-  //   }
-  //   //Check for files present in the additional media maps that are not present on the filesystem
-  //   for (const [congregation, additionalMediaMap] of Object.entries(
-  //     additionalMediaMaps.value,
-  //   )) {
-  //     for (const [dateKey, mediaObjects] of Object.entries(
-  //       additionalMediaMap,
-  //     )) {
-  //       for (const mediaObject of mediaObjects) {
-  //         const filePath = path.resolve(fileUrlToPath(mediaObject.fileUrl));
-  //         if (!fs.existsSync(filePath)) {
-  //           additionalMediaMaps.value[congregation][dateKey] =
-  //             mediaObjects.filter((obj) => obj.fileUrl !== mediaObject.fileUrl);
-  //         }
-  //       }
-  //     }
-  //   }
-  //   removeEmptyDirs(additionalMediaPath);
-  // } catch (error) {
-  //   errorCatcher(error);
-  // }
+  try {
+    const jwStore = useJwStore();
+    const { additionalMediaMaps } = storeToRefs(jwStore);
+    const additionalMediaPath = getAdditionalMediaPath();
+    if (!fs.existsSync(additionalMediaPath)) {
+      additionalMediaMaps.value = {};
+      return;
+    }
+    const flattenedFilePaths = (
+      data: Record<string, Record<string, DynamicMediaObject[]>>,
+    ) => {
+      return Object.values(data).flatMap((dateObj) =>
+        Object.values(dateObj).flatMap((files) =>
+          files.map((file) => path.resolve(fileUrlToPath(file.fileUrl))),
+        ),
+      );
+    };
+    // Check for files present on the filesystem that are not present in the additional media maps
+    const filesReferencedInAdditionalMediaMaps = flattenedFilePaths(
+      additionalMediaMaps.value,
+    );
+    const dirListing = klawSync(additionalMediaPath, { nodir: true }).map((f) =>
+      path.resolve(f.path),
+    );
+    console.log('flattenedFilePaths:', filesReferencedInAdditionalMediaMaps);
+    console.log('dirListing:', dirListing);
+    for (const additionalMediaFilePath of dirListing) {
+      if (
+        !filesReferencedInAdditionalMediaMaps.includes(additionalMediaFilePath)
+      ) {
+        console.log(
+          'Removing orphaned file from filesystem:',
+          additionalMediaFilePath,
+        );
+        fs.rmSync(additionalMediaFilePath);
+      }
+    }
+
+    // Check for files present in the additional media maps that are not present on the filesystem
+    for (const [congregation, additionalMediaMap] of Object.entries(
+      additionalMediaMaps.value,
+    )) {
+      for (const [dateKey, mediaObjects] of Object.entries(
+        additionalMediaMap,
+      )) {
+        for (const mediaObject of mediaObjects) {
+          const filePath = path.resolve(fileUrlToPath(mediaObject.fileUrl));
+          if (!fs.existsSync(filePath)) {
+            console.log(
+              'Removing orphaned file from additional media maps:',
+              filePath,
+            );
+            additionalMediaMaps.value[congregation][dateKey] =
+              mediaObjects.filter((obj) => obj.fileUrl !== mediaObject.fileUrl);
+          }
+        }
+      }
+    }
+    removeEmptyDirs(additionalMediaPath);
+  } catch (error) {
+    errorCatcher(error);
+  }
 };
 
 export { cleanAdditionalMediaFolder, cleanLocalStorage };
