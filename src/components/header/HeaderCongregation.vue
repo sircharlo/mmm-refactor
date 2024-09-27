1ad4a8ac75ddef84e86883c8fa860cf6deeeee94
Manoah Tervoort <149895ja@gmail.com>
2024-09-27 15:36:22 +0200

    refactor: separate headers

---
+++ src/components/header/HeaderCongregation.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <q-btn
+      :label="$t('new-profile')"
+      color="white-transparent"
+      icon="mmm-plus"
+      unelevated
+      @click="createNewCongregation"
+    />
+  </div>
+</template>
+<script setup lang="ts">
+const createNewCongregation = () => {
+  window.dispatchEvent(new CustomEvent('createNewCongregation'));
+};
+</script>
