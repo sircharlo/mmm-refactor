6c9faf10f4f4442d42a35801ac92399956ff1fc3
Manoah Tervoort <149895ja@gmail.com>
2024-10-11 19:46:51 +0200

    chore: cleanup

--- src/composables/useLocale.ts
+++ src/composables/useLocale.ts
@@ -1,12 +1,11 @@
 import type { DateLocale } from 'quasar';
-import type { MessageLanguages } from 'src/boot/i18n';
 
-import { computed, nextTick } from 'vue';
+import { computed } from 'vue';
 import { useI18n } from 'vue-i18n';
 
 export function useLocale() {
   const i18n = useI18n();
-  const { availableLocales, locale, setLocaleMessage, t } = i18n;
+  const { t } = i18n;
 
   const dateLocale = computed((): Required<DateLocale> => {
     return {
@@ -17,23 +16,5 @@ export function useLocale() {
     };
   });
 
-  async function loadI18nMessages(lang: MessageLanguages) {
-    if (!availableLocales.includes(lang)) {
-      const messages = await import(`./../i18n/${lang}.json`);
-      setLocaleMessage(lang, messages.default);
-      await nextTick();
-    }
-  }
-
-  async function switchLocale(newLocale: MessageLanguages) {
-    console.log('Switching locale to:', newLocale);
-    if (!availableLocales.includes(newLocale)) {
-      await loadI18nMessages(newLocale);
-    }
-
-    locale.value = newLocale;
-    document.querySelector('html')?.setAttribute('lang', newLocale);
-  }
-
-  return { dateLocale, locale, switchLocale, t };
+  return { dateLocale };
 }
