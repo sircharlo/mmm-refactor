ec110abc0f17c9dd62be01e8955cfc8b341dd8ae
Olivier Savignac <1275666+sircharlo@users.noreply.github.com>
2024-10-14 10:24:20 -0400

    Revert "fix: improve logic of getSpecificWeekday function"

--- src/helpers/date.ts
+++ src/helpers/date.ts
@@ -90,24 +90,15 @@ const getWeekDay = (lookupDate: Date) => {
   }
 };
 
-function getSpecificWeekday(lookupDate: Date, desiredWeekday: number): Date {
+function getSpecificWeekday(date: Date, desiredWeekday: number) {
   try {
-    if (!lookupDate) throw new Error('No lookupDate provided');
-
-    // Get the day of the week for the lookupDate (0 = Sunday, 6 = Saturday)
-    const currentWeekday = lookupDate.getDay();
-
-    // If Sunday, subtract one week
-    if (currentWeekday === 0) {
-      lookupDate.setDate(lookupDate.getDate() - 7);
-    }
-
-    // Calculate difference between current day and desired day in the same week
-    const difference = desiredWeekday - currentWeekday;
-
-    // Adjust the date
-    const newDate = new Date(lookupDate);
-    newDate.setDate(lookupDate.getDate() + difference);
+    if (!date) throw new Error('No date');
+    if (desiredWeekday == null) throw new Error('No desired weekday');
+    desiredWeekday++;
+    desiredWeekday = desiredWeekday === 7 ? 0 : desiredWeekday;
+    const difference = (date.getDay() - desiredWeekday + 7) % 7;
+    const newDate = new Date(date.valueOf());
+    newDate.setDate(newDate.getDate() - difference);
     return newDate;
   } catch (error) {
     errorCatcher(error);
@@ -133,18 +124,8 @@ function isCoWeek(lookupDate: Date) {
     const coWeekSet = !!currentSettings.value?.coWeek;
     if (!coWeekSet) return false;
     const coWeekTuesday = dateFromString(currentSettings.value?.coWeek);
-    const coMonday = getSpecificWeekday(coWeekTuesday, 1);
-    const lookupWeekMonday = getSpecificWeekday(lookupDate, 1);
-    console.debug(
-      'lookupDate',
-      lookupDate,
-      'coWeekTuesday',
-      coWeekTuesday,
-      'coMonday',
-      coMonday,
-      'lookupWeekMonday',
-      lookupWeekMonday,
-    );
+    const coMonday = getSpecificWeekday(coWeekTuesday, 0);
+    const lookupWeekMonday = getSpecificWeekday(lookupDate, 0);
     return datesAreSame(coMonday, lookupWeekMonday);
   } catch (error) {
     errorCatcher(error);
